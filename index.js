bbdfec00bcc0d4fb837adbfce017e5bc041fede7
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2020-03-17 21:36:13 -0700

    updates

--- index.js
+++ index.js
@@ -27,6 +27,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 
 app.use('/', indexRouter);
 app.use('/api/signup', signUp);
